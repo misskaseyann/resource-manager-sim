76a1d91e86136bb8b5f64e82a9dd4ac917335dbc
Kasey Ann <kaseystowell@Kaseys-MacBook-Pro.local>
2018-10-28 14:56:06 -0400

    added model part and folder for inputs

--- program3/core.py
+++ program3/core.py
@@ -4,3 +4,9 @@
 class Core(object):
     def __init__(self):
         print("sup")
+
+    def read_file(self, fp):
+        try:
+            f = open(fp,'r')
+        except IOError:
+            print("Cannot find the file at ", fp)
